7bc176b9f7e03f8c17cd5204c37ce28969b6a511
Robert Mastalerek <rmastalerek@consdata.com>
2026-06-03 12:28:41 +0200

    incident meter tags fix

--- extension/src/main/java/org/eximeebpms/bpm/extension/monitor/IncidentMeterTags.java
+++ extension/src/main/java/org/eximeebpms/bpm/extension/monitor/IncidentMeterTags.java
@@ -9,30 +9,34 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum IncidentMeterTags {
 
-    TENANT_ID("tenant.id"), PROCESS_DEFINITION_ID("process.definition.id"),
-    PROCESS_DEFINITION_KEY("process.definition.key"), ACTIVITY_ID("activity.id"),
-    FAILED_ACTIVITY_ID("failed.activity.id"), INCIDENT_TYPE("incident.type");
+  TENANT_ID("tenant.id"), PROCESS_DEFINITION_ID("process.definition.id"),
+  PROCESS_DEFINITION_KEY("process.definition.key"), ACTIVITY_ID("activity.id"),
+  FAILED_ACTIVITY_ID("failed.activity.id"), INCIDENT_TYPE("incident.type");
 
-    private final String tagName;
+  private final String tagName;
 
-    public static Tags createTags(String tenantId, String processDefinitionId, String processDefinitionKey,
-            String activityId, String failedActivityId, String incidentType) {
-        Tags tags = Tags.empty();
+  public static Tags createTags(String tenantId, String processDefinitionId, String processDefinitionKey,
+      String activityId, String failedActivityId, String incidentType) {
+    Tags tags = Tags.empty();
 
-        if (tenantId != null) {
-            tags = tags.and(IncidentMeterTags.TENANT_ID.getTagName(), tenantId);
-        }
-
-        tags = tags.and(IncidentMeterTags.PROCESS_DEFINITION_ID.getTagName(), processDefinitionId);
-        tags = tags.and(IncidentMeterTags.PROCESS_DEFINITION_KEY.getTagName(), processDefinitionKey);
-        tags = tags.and(IncidentMeterTags.ACTIVITY_ID.getTagName(), activityId);
-        if (!Objects.equals(tenantId, failedActivityId)) {
-            tags = tags.and(IncidentMeterTags.FAILED_ACTIVITY_ID.getTagName(), failedActivityId);
-        }
-        tags = tags.and(IncidentMeterTags.INCIDENT_TYPE.getTagName(), incidentType);
-
-        return tags;
-
+    if (tenantId != null) {
+      tags = tags.and(IncidentMeterTags.TENANT_ID.getTagName(), tenantId);
     }
+
+    tags = addIfNotNull(tags, IncidentMeterTags.PROCESS_DEFINITION_ID, processDefinitionId);
+    tags = addIfNotNull(tags, IncidentMeterTags.PROCESS_DEFINITION_KEY, processDefinitionKey);
+    tags = addIfNotNull(tags, IncidentMeterTags.ACTIVITY_ID, activityId);
+    if (failedActivityId != null && !Objects.equals(activityId, failedActivityId)) {
+      tags = tags.and(IncidentMeterTags.FAILED_ACTIVITY_ID.getTagName(), failedActivityId);
+    }
+    tags = addIfNotNull(tags, IncidentMeterTags.INCIDENT_TYPE, incidentType);
+
+    return tags;
+
+  }
+
+  private static Tags addIfNotNull(Tags tags, IncidentMeterTags tag, String value) {
+    return value == null ? tags : tags.and(tag.getTagName(), value);
+  }
 
 }
